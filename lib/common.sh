0da0ede5bbc2826053f4ca77de5cb821168f3a8c
sudeepdino008 <sudeepdino008@gmail.com>
2026-02-24 00:50:22 +0530

    common: add CYAN color variable

--- lib/common.sh
+++ lib/common.sh
@@ -8,12 +8,14 @@ if [[ -t 1 ]]; then
     RED='\033[0;31m'
     GREEN='\033[0;32m'
     YELLOW='\033[0;33m'
+    CYAN='\033[0;36m'
     BOLD='\033[1m'
     RESET='\033[0m'
 else
     RED=''
     GREEN=''
     YELLOW=''
+    CYAN=''
     BOLD=''
     RESET=''
 fi
